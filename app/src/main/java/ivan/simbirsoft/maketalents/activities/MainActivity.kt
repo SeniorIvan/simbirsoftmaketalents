0eda849f0a382bc3580b4da24e67baadd2bbaf33
Ivan Kuz <boytheprodigy@gmail.com>
2018-03-17 20:14:28 +0400

    Initial commit

---
+++ app/src/main/java/ivan/simbirsoft/maketalents/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package ivan.simbirsoft.maketalents.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ivan.simbirsoft.maketalents.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
