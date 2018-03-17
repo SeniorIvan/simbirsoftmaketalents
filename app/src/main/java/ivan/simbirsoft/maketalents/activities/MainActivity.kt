c27cf8d535d87eedc52ec94748f81897f893ab0d
Ivan Kuz <boytheprodigy@gmail.com>
2018-03-17 20:27:10 +0400

    include firebase auth
    include firebase auth

--- app/src/main/java/ivan/simbirsoft/maketalents/activities/MainActivity.kt
+++ app/src/main/java/ivan/simbirsoft/maketalents/activities/MainActivity.kt
@@ -1,7 +1,12 @@
 package ivan.simbirsoft.maketalents.activities
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import ivan.simbirsoft.maketalents.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +14,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val providers = listOf((AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
+
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+
+                if (user != null) {
+                    Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "error", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "error", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
